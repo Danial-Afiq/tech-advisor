e849d6a84364df5492d6475630eac9dedfb911ae
Zayna Ng <zayna.ng.2025@computing.smu.edu.sg>
2026-09-18 23:16:03 +0800

    fix: use delegating password encoder

--- backend/src/main/java/com/springboot/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/springboot/backend/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.springboot.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,9 +11,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        public PasswordEncoder passwordEncoder() {
+                return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
